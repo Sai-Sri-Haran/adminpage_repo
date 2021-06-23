a6fef1e08e36a5780734b9f3a7e53e91cf2be0bf
Sai-SriHaran-codingmart <sai.sriharan@codingmart.com>
2021-06-23 12:49:50 +0530

    admin deploy

--- db/schema.rb
+++ db/schema.rb
@@ -47,13 +47,11 @@ ActiveRecord::Schema.define(version: 2021_06_18_045708) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string "orderid"
-    t.string "product"
-    t.bigint "showusers_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["showusers_id"], name: "index_orders_on_showusers_id"
+  create_table "orders", primary_key: "order_id", id: :serial, force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.string "email", limit: 255, null: false
+    t.string "status", limit: 50, null: false
+    t.datetime "created_on", null: false
   end
 
   create_table "products", force: :cascade do |t|
@@ -100,5 +98,4 @@ ActiveRecord::Schema.define(version: 2021_06_18_045708) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "orders", "showusers", column: "showusers_id"
 end
